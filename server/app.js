dcd5b8e02e7faa1a39d5bcb667d6d0463900bfb7
yabeshan <beshan@bigmir.net>
2020-07-22 11:39:36 +0200

    'Fixing'

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const port = process.env.PORT || 3000
+
+http.createServer((req, res) => {
+  res.writeHead(200)
+  res.end("Hello")
+}).listen(port, err => {
+  if (err) {
+    throw err
+  }
+  console.log(`> Ready on port:${port}`)
+})
